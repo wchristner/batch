b9ec773ba35bf1d4fb9ee412138448c48b7afdb5
William Christner <wchristner@gmail.com>
2021-08-24 01:09:05 -0400

    Add files via upload
    Batch file to get Hardware and Software information and save to the desktop in two text files.

---
+++ sysinfo.bat
@@ -0,0 +1,25 @@
+@echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+@Title Cluster Flux Technologies System Info
+@echo.
+@echo.
+systeminfo.exe>C:\hardinfo.txt
+wmic product get Name, Version >> c:\softinfo.txt 
+move /y C:\hardinfo.txt C:\Users\Public\Desktop
+move /y C:\softinfo.txt C:\Users\Public\Desktop
